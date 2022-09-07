3241540fad9fd47658f120888042218ab65883f1
MishaZalishchuk <misha.zalishuk@gmail.com>
2022-09-07 13:34:25 +0300

    last changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,20 @@ $(document).ready(function () {
         $('.navbar-demo').toggleClass('open-nav');
     });
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:1
+        }
+    }
+});
